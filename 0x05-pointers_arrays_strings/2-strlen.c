4fa67af5f126654f8e88d8448c99d33e1f3426e0
Tinodana <abinarac@gmail>
2023-03-27 06:43:55 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the string length
+ * @s: string whose length to be printed
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
